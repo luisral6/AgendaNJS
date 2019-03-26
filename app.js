21b01121b79f65bc63eb12c1e7c95231ac30ca64
Luis Arriola <luis.arriola6@gmail.com>
2019-03-26 02:38:01 -0700

    WORKS

--- app.js
+++ app.js
@@ -1,80 +1,139 @@
 const express = require('express');
-const bodyParser = require( 'body-parser');
+const bodyParser = require("body-parser");
+const path = require('path');
+const Joi = require('joi');
+
+const db = require("./db");
+const collection = "todo";
 const app = express();
+
+// schema used for data validation for our todo document
+const schema = Joi.object().keys({
+    name : Joi.string().required(),
+    lastName : ,
+    address : ,
+    email : ,
+    phone : ,
+    phoneType :,
+
+});
+
+// parses json data sent to us by the user
 app.use(bodyParser.json());
-const path =require('path');
 
-const db = require('./db');
-const collection ="todo";
-
-//redirection to homepage
+// serve static html file to user
 app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname,'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
-
+// read
 app.get('/getTodos',(req,res)=>{
-  db.getDB().collection(collection).find({}).toArray((err,documents)=>{
-      if (!err) {
-          console.log(documents);
-          res.json(documents);
-
-      } else {
-          console.log(err);
-      }
-  })
+    // get all Todo documents within our todo collection
+    // send back to user as json
+    db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(documents);
+        }
+    });
 });
 
-app.put('/:id',(req,res)=>{
+// update
+app.put('/:id',(req,res,next)=>{
     // Primary Key of Todo Document we wish to update
     const todoID = req.params.id;
     // Document used to update
     const userInput = req.body;
     // Find Document By ID and Update
-    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)},{$set : {todo : userInput.todo}},{returnOriginal : false},(err,result)=>{
+
+    Joi.validate(userInput,schema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else{
+            db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)},{$set : {todo : userInput.todo}},{returnOriginal : false},(err,result)=>{
+                if(err)
+                    console.log(err);
+                else{
+                    res.json(result);
+                }
+            });
+        }
+    })
+
+});
+
+
+//create
+app.post('/',(req,res,next)=>{
+    // Document to be inserted
+    const userInput = req.body;
+
+    // Validate document
+    // If document is invalid pass to error middleware
+    // else insert document within todo collection
+    Joi.validate(userInput,schema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else{
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err){
+                    const error = new Error("Failed to insert Todo Document");
+                    error.status = 400;
+                    next(error);
+                }
+                else
+                    res.json({result : result, document : result.ops[0],msg : "Successfully inserted Todo!!!",error : null});
+            });
+        }
+    })
+});
+
+
+
+//delete
+app.delete('/:id',(req,res)=>{
+    // Primary Key of Todo Document
+    const todoID = req.params.id;
+    // Find Document By ID and delete document from record
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)},(err,result)=>{
         if(err)
             console.log(err);
-        else{
+        else
             res.json(result);
-        }
     });
 });
 
-app.post('/',(req,res)=>{
-    const userInput =req.body;
-    db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.json({result: result, document:result.ops[0]})
+// Middleware for handling Error
+// Sends Error Response Back to User
+app.use((err,req,res,next)=>{
+    res.status(err.status).json({
+        error : {
+            message : err.message
         }
     });
-});
-
-
-app.delete('/:id',(req,res)=>{
-    const todoID = req.params.id;
-    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)},(err,result)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.json(result);
-        }
-    });
-});
-
-
-
-// db connection
-db.connect((err)=>{
-  if(err){
-    console.log(err);
-    console.log("Unable to connect to db");
-    process.exit(1 );
-  }else{
-    app.listen(3000,()=>{
-      console.log("Connected to db, on port 3000");
-    });
-  }
 })
 
+
+db.connect((err)=>{
+    // If err unable to connect to database
+    // End application
+    if(err){
+        console.log('unable to connect to database');
+        process.exit(1);
+    }
+    // Successfully connected to database
+    // Start up our Express Application
+    // And listen for Request
+    else{
+        app.listen(3000,()=>{
+            console.log('connected to database, app listening on port 3000');
+        });
+    }
+});
